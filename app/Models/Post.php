e2d42c7cd9007fbef9e8911f4a5ef6e034d3daa5
allyssonrodrigues <allysson.r@outlook.com>
2024-07-24 20:56:24 -0300

    TerceiraAula

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,32 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    // /** Quando precisar usar outro nome da tabela no banco*/
+    // protected $table = 'postagens';
+
+    // /** Quando quiser usar outro nome no campo ID da tabela */
+    // protected $primaryKey = 'id_postagens';
+
+    // /** Muda o tipo da chave ID */
+    // protected $keyType = 'string';
+
+    // /** Desabilita o AutoIncremente da Tabela */
+    // protected $incrementing = false;
+
+    // /** Desabilitar o campo TimesTamps */
+    // protected $timestamps = true;
+
+    // const CREATED_AT = 'data_criacao';
+    // const UPDATED_AT = 'data_atualizacao';
+
+    // protected $dateFormat = 'd-m-Y';
+
+    // /** Definir Model para utilizar outro banco */
+    // protected $connection = 'pgsql';
+
+    // /** Definir um valor default para os atributos */
+    // protected $attributes = [ 
+    //     'title' => 'Testando a aplicação Laravel'
+    // ];
 }
