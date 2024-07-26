6c088d6229ed35b075550b5d33ba793cbb9d864c
allyssonrodrigues <allysson.r@outlook.com>
2024-07-25 21:48:16 -0300

    Aula 3,4 e 5

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,57 @@
 <?php
 
+use App\Models\Post;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
+
+Route::get('/insert', function (Post $post) {
+   
+    $post->user_id = 1;
+    $post->title = 'Testando' . Str::random(10);
+    $post->body = '12x34';
+    $post->date = date('Y-m-d');
+
+    $post->save();
+
+    $posts = $post->get();
+    return $posts;
+});
+
+Route::get('/orderby', function (User $user) {
+   
+    $users = $user->orderBy('name', 'desc')->get();
+
+    
+    return $users;
+});
+
+Route::get('/pagination', function (User $user) {
+   
+    $users = $user->paginate(10);
+
+    /** Paginate no Blade */
+    // {{$users->links()}}
+
+    
+    return $users;
+});
+
+Route::get('/where', function (User $user) {
+    $filter = 'Carlos';
+    //$users = $user->where('email', '=', 'camille.gaylord@example.net')->first();
+    //$users = $user->where('email', 'camille.gaylord@example.net')->first();
+
+    //$users = $user->where('name', 'LIKE', "%{$filter}%")->orWhere('name', 'LIKE', '%Ku%')->first();
+
+    $users = $user->where('email', 'camille.gaylord@example.net')
+                    ->orWhere(function($query){
+                        $query->where('name', '<>', 'Carlos');
+                    })->first();
+
+    dd($users);
+});
 
 Route::get('/select', function(){
     //$users = User::all();
